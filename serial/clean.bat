99eceab04f0b4dffaae0dac7d44608dca23f1e93
Sebastien <mrtseb14@gmail.com>
2020-02-18 21:08:20 +0100

    Signed-off-by: Sebastien <mrtseb14@gmail.com>

--- serial/clean.bat
+++ serial/clean.bat
@@ -1,11 +1,8 @@
 del /s /q *.~*
-del /s /q *.raw
-del /s /q *.cir
 del /s /q *.dcu
-del /s /q *.log
 del /s /q *.ppu
 del /s /q *.bak
 del /s /q *.obj
-
-strip.exe CeC1.exe
+del /s /q lib\*.*
+del /s /q backup\*.*
 
